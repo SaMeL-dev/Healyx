575f7349bdf1d7264a07996b8ad69a55751fb05a
ljsoung <super15600@gmail.com>
2026-04-16 21:41:35 +0900

    backend first setting

---
+++ backend/Healyx/src/main/java/com/smu/healyx/HealyxApplication.java
@@ -0,0 +1,13 @@
+package com.smu.healyx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealyxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealyxApplication.class, args);
+	}
+
+}
---
+++ backend/Healyx/src/test/java/com/smu/healyx/HealyxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smu.healyx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealyxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
